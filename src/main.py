11007336055d04834b29ffeacbdfcd6fe9da7536
Stefanie <steffi@Stefanies-MBP.fritz.box>
2020-11-27 17:27:51 +0100

    Renamed some parameters, added comments

--- src/main.py
+++ src/main.py
@@ -12,41 +12,75 @@ matplotlib_tuda.load()
 
 
 def denoise_audio(signal: np.ndarray) -> np.ndarray:
+    """
+    :TODO: recognize the noise to automate the denoising.
+    Denoises given audio signal.
+    :param signal: audio signal as int16 values
+    :returns: denoised audio signal as int16 values
+    """
     noisy_part = signal[:3 * 44100]
     # perform noise reduction
     reduced_noise = nr.reduce_noise(audio_clip=signal.astype(np.float16), noise_clip=noisy_part.astype(np.float16), verbose=False).astype(np.int16)
     return reduced_noise
 
 
-def read_audio(filename: str) -> tuple[np.ndarray, np.ndarray, float]:
+def read_audio(filename: str) -> Tuple[np.ndarray, np.ndarray, float]:
+    """
+    :TODO: make it work with stereo/mono
+    Reads Wave-Files 
+    :param filename: path to file from media folder, without the .wav ending
+    :returns: shape:(T,) all the sampled times as floats
+    :returns: shape:(S,) the samples of the left stereoline as int16 values
+    :returns: samplerate as float
+    """
     rate, data = scipy.io.wavfile.read(f'../media/{filename}.wav')
     time_step = 1 / rate
     time_vec = np.arange(0, data.shape[0]) * time_step
     return time_vec, data[:, 0], rate
 
 
-def generate_exemplary_audio() -> tuple[np.ndarray, np.ndarray, float]:
+def generate_exemplary_audio() -> Tuple[np.ndarray, np.ndarray, float]:
+    """
+    :TODO: make it work with stereo/mono
+    :TODO: take params for signalfunctions
+    Generates exemplary audio signal
+    :returns: shape:(T,) all the sampled times as floats
+    :returns: shape:(S,) the samples of the left stereoline as int16 values
+    :returns: samplerate as float
+    """
     time_step = .01
     time_vec = np.arange(0, 70, time_step)
     signal = np.sin(0.5 * np.pi * time_vec * (1 + .1 * time_vec))
     return time_vec, signal, 1 / time_step
 
 
-def plot_signal(time_vec: np.ndarray, signal: np.ndarray, filename: str) -> None:
+def plot_signal(time_vec: np.ndarray, signal: np.ndarray, caption: str) -> None:
+    """
+    plots the audio signal
+    :param time_vec: all the sampled times as floats
+    :param signal: audio signal as int16 values
+    :param caption: The caption for the plot as String
+    """
     fig, ax = plt.subplots()
-    ax.set_title(f'Signal ({filename})')
+    ax.set_title(f'Signal ({caption})')
     ax.plot(time_vec, signal)
     fig.show()
 
 
-def plot_spectrogram(signal: np.ndarray, frequency: float, filename: str) -> None:
-    freqs, times, spectrogram = scp.signal.spectrogram(signal, fs=frequency)
+def plot_spectrogram(signal: np.ndarray, rate: float, caption: str) -> None:
+    """
+    Plots the spectogram.
+    :param signal: audio signal as int16 values
+    :param rate: samplerate as float
+    :param caption: The caption for the plot as String
+    """
+    freqs, times, spectrogram = scp.signal.spectrogram(signal, fs=rate)
 
     fig, ax = plt.subplots()
     tmp = ax.contourf(times, freqs, spectrogram, 10.0 ** np.arange(-6, 6), locator=ticker.LogLocator(), cmap=plt.get_cmap('jet'))
     # plt.clim(10^(-12),10)
     fig.colorbar(tmp)
-    ax.set_title(f'Spectrogram ({filename})')
+    ax.set_title(f'Spectrogram ({caption})')
     ax.set_ylabel('Frequency band')
     # ax.set_yscale('log')
     plt.ylim(ymax=10000, ymin=0)
@@ -67,7 +101,6 @@ def main():
         plot_signal(time_vec, denoised_signal, filename + '_den')
         plot_spectrogram(denoised_signal, rate, filename + '_den')
 
-        # Generate noise only. Not working yet.
         noise_signal = signal - denoised_signal
         scipy.io.wavfile.write(f'../media/{filename}_noiseonly_generated.wav', rate, noise_signal)
         plot_signal(time_vec, noise_signal, filename + '_noise')
@@ -78,6 +111,8 @@ def main():
         time_vec, signal, rate = read_audio(filename)  # generate_exemplary_audio()
         # plot_signal(time_vec, signal, filename)
         plot_spectrogram(signal, rate, filename)
+    
+    input('Press enter to finish.')
 
 
 if __name__ == '__main__':
